8ed5ae3553688a9bef89c3661136d95c5f77ea54
Rebecca Ackerman <rcackerman@gmail.com>
2019-04-24 17:28:24 -0400

    Using the full database name

--- request.sql
+++ request.sql
@@ -1,10 +1,19 @@
 /****** Script for selecting NYSIDs to send to DOC  ******/
 
-SELECT
-	nam_NYSID
-FROM Fil_Names
+SELECT DISTINCT
+	[nam_NYSID] AS NYSID
+FROM [NYPDCMS].[dbo].[Fil_Names]
 WHERE EXISTS (
 	SELECT 1
-	FROM Fil_Cases
-	WHERE Fil_Names.Alias_Link = Fil_Cases.AliasID
-	AND (Fil_Cases.cas_Case_Status = 'O' | Fil_Cases.cas_Case_Status = 'I');
+	FROM [NYPDCMS].[dbo].[Fil_Cases]
+	WHERE [Fil_Names].[nam_Alias_Link] = [Fil_Cases].[cas_AliasID]
+  -- Look for only open cases OR
+	AND ([Fil_Cases].[cas_Case_Status] = 'O'
+      -- Cases that bench warranted AND were a felony
+      -- otherwise we can assume that another office will
+      -- just take the case.
+        OR ([Fil_Cases].[cas_Case_Status] = 'I'
+            AND [Fil_Cases].[cas_Case_Detail] = 'Felony')
+	)
+)
+;
