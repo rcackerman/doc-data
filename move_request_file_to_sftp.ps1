4b401d3a306199ccd58656e86dbf3fd149280178
Rebecca Ackerman <rcackerman@gmail.com>
2019-05-16 17:11:53 -0400

    Finding the most recently created CSV to upload

--- move_request_file_to_sftp.ps1
+++ move_request_file_to_sftp.ps1
@@ -1,18 +1,22 @@
 $DOCPath = $PSScriptRoot
-$RequestsFolder = Join-Path -Path $DOCPath -ChildPath "requests"
-$OutfileName = Join-Path -Path $RequestsFolder -ChildPath "NYCOUNTY_REQUEST_$((Get-Date).toString("yyyyMMdd")).csv"
+$config = ([xml](Get-Content (Join-Path -Path $DOCPath -ChildPath "config.xml"))).config
+$RequestsFolder = Join-Path $DOCPath "requests"
+
+# Select the most recently written file in the requests directory
+$RequestFileName = Join-Path $RequestsFolder (Get-ChildItem "$($RequestsFolder)/*.csv" | Sort-Object LastWriteTime | Select-Object -Last 1).name
 
 $credential = Get-Credential
-$sessionOption = New-WinSCPSessionOption -HostName <> -Protocol Sftp -Credential $credential
+$sessionOption = New-WinSCPSessionOption -HostName $config.sftp.HostName -Protocol Sftp -Credential $credential
 
-
+##
+# Start the SFTP session
 New-WinSCPSession -SessionOption $sessionOption
 
 # Remove old files
-Remove-WinSCPItem -Path (Join-Path -Path $config.sftp.SFTPRequestFolder -ChildPath "*.csv")
+Remove-WinSCPItem -Path (Join-Path $config.sftp.SFTPRequestFolder "*.csv")
 # Upload new request file
-Send-WinSCPItem -Path $OutfileName -Destination $config.sftp.SFTPRequestFolder
+Send-WinSCPItem -Path $RequestFileName -Destination $config.sftp.SFTPRequestFolder
 
 Remove-WinSCPSession
-
-
+# End the SFTP session
+##
