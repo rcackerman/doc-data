2f0fdc8b3c68c6f5732fe3157b6810fc22a7022b
Rebecca Ackerman <rcackerman@gmail.com>
2019-05-16 10:38:50 -0400

    Saving with Unix-style line endings

--- create_request_file.ps1
+++ create_request_file.ps1
@@ -13,3 +13,15 @@ Invoke-Sqlcmd @sqlParams -AbortOnError -InputFile $infileName | Out-File -FilePa
 $nysids = Get-Content $outfileName
 $cleanedNysids = ( Get-Content $outfileName | Select-Object -Skip 4 ) | Foreach {$_.TrimEnd()}
 Set-Content -Value $cleanedNysids -Path $outfileName
+
+# Convert CRLFs to LFs only.
+# Note:
+#  * (...) around Get-Content ensures that $file is read *in full*
+#    up front, so that it is possible to write back the transformed content
+#    to the same file.
+#  * + "`n" ensures that the file has a *trailing LF*, which Unix platforms
+#     expect.
+# From this post: https://stackoverflow.com/a/19132572/702383
+$text = [IO.File]::ReadAllText($outfileName) -replace "`r`n", "`n"
+[IO.File]::WriteAllText($outfileName, $text)
+Set-Content $outfileName -Encoding Ascii -Value $text
