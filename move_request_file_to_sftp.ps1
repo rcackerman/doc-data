a50f1158bc68ad1ce025414a4993634abb3e5db0
Rebecca Ackerman <rcackerman@gmail.com>
2019-05-16 16:55:58 -0400

    Finishing up the uploading stage

--- move_request_file_to_sftp.ps1
+++ move_request_file_to_sftp.ps1
@@ -5,5 +5,14 @@ $OutfileName = Join-Path -Path $RequestsFolder -ChildPath "NYCOUNTY_REQUEST_$((G
 $credential = Get-Credential
 $sessionOption = New-WinSCPSessionOption -HostName <> -Protocol Sftp -Credential $credential
 
+
 New-WinSCPSession -SessionOption $sessionOption
-Send-WinSCPItem -Path $OutfileName -Destination '<>'
+
+# Remove old files
+Remove-WinSCPItem -Path (Join-Path -Path $config.sftp.SFTPRequestFolder -ChildPath "*.csv")
+# Upload new request file
+Send-WinSCPItem -Path $OutfileName -Destination $config.sftp.SFTPRequestFolder
+
+Remove-WinSCPSession
+
+
