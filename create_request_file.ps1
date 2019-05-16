b6eb8a54e1b6a288b355109622ed1d5afe3212e7
Rebecca Ackerman <rcackerman@gmail.com>
2019-05-16 10:35:31 -0400

    Cleaning up the command

--- create_request_file.ps1
+++ create_request_file.ps1
@@ -3,5 +3,8 @@ $RequestsFolder = Join-Path -Path $DOCPath -ChildPath "requests"
 $infileName = Join-Path -Path $DOCPath -ChildPath "request.sql"
 $outfileName = Join-Path -Path $RequestsFolder -ChildPath "NYCOUNTY_REQUEST_$((Get-Date).toString("yyyyMMdd")).csv"
 
+$sqlParams = @{ HostName = "PDCMS2"
+                Database = "NYPDCMS" }
+
 # Query for NYSIDs returns data, then pipe to the outfile in ascii format
-Invoke-Sqlcmd -HostName "PDCMS2" -Database "NYPDCMS" -AbortOnError -InputFile $infileName | Out-File -FilePath $outfileName -Encoding ascii
+Invoke-Sqlcmd @sqlParams -AbortOnError -InputFile $infileName | Out-File -FilePath $outfileName -Encoding ascii
