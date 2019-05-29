b4595c39347e87ccf4c5cd0711c857346757beb9
Rebecca Ackerman <rcackerman@gmail.com>
2019-05-29 10:11:32 -0400

    Adding credentials so the SFTP connection works

--- move_request_file_to_sftp.ps1
+++ move_request_file_to_sftp.ps1
@@ -5,15 +5,15 @@ $RequestsFolder = Join-Path $DOCPath "requests"
 # Select the most recently written file in the requests directory
 $RequestFileName = Join-Path $RequestsFolder (Get-ChildItem "$($RequestsFolder)/*.csv" | Sort-Object LastWriteTime | Select-Object -Last 1).name
 
-$credential = Get-Credential
-$sessionOption = New-WinSCPSessionOption -HostName $config.sftp.HostName -Protocol Sftp -Credential $credential
+$credential = New-Object System.Management.Automation.PSCredential $config.sftp.Username, (ConvertTo-SecureString $config.sftp.Password -AsPlainText -Force)
+$sessionOption = New-WinSCPSessionOption -HostName $config.sftp.HostName -Protocol Sftp -Credential $credential -GiveUpSecurityAndAcceptAnySshHostKey
 
 ##
 # Start the SFTP session
 New-WinSCPSession -SessionOption $sessionOption
 
 # Remove old files
-Remove-WinSCPItem -Path (Join-Path $config.sftp.SFTPRequestFolder "*.csv")
+Remove-WinSCPItem -Path (Join-Path $config.sftp.SFTPResponseFolder "*.csv") 
 # Upload new request file
 Send-WinSCPItem -Path $RequestFileName -Destination $config.sftp.SFTPRequestFolder
 
